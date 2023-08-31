7f0f2f9aadfcaef273439cb189a55a36c90d1462
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-31 14:18:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
-from langchain.chat_models import OpenAI, AgentType
+
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.agents.agent_types import AgentType
 from langchain.agents import create_csv_agent
 
 import os
